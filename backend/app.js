2b33ef70098671f93a891675672a31c492576da1
Qiyao Liu <liu@Qiyaos-MacBook-Pro.local>
2024-09-05 18:02:30 +0200

    cleared bug

--- backend/app.js
+++ backend/app.js
@@ -12,7 +12,7 @@ const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
-const reviewRouter = require('./routes/reviewRoutestra');
+const reviewRouter = require('./routes/reviewRoutes');
 
 const transactionController = require('./controllers/transactionController');
 
